c1ad8dd1286db980689317d790a97bb8548fb399
Maks-eco <maks.zawulon@gmail.com>
2024-12-23 02:16:58 +0400

    Firebase added

---
+++ src/components/NameInput.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <input type="text" v-model="name" />
+        <span>{{ name }}</span> <br />
+        <span>{{ name ? 'Youre imya: ' + name : 'Vvedite imya' }}</span> <br />
+        <button @click="saveName">Store</button>
+    </div>
+</template>
+
+<script lang="ts" setup>
+// const numberOfPurchases = useCounterStore()
+import { ref, onMounted } from 'vue'
+const name = ref('')
+const nameCode = ref('')
+const saveName = () => {
+    localStorage.setItem('myname', name.value)
+}
+
+onMounted(() => {
+    const nameStor = localStorage.getItem('myname')
+    if (nameStor) {
+        name.value = nameStor
+    }
+})
+</script>
+
+<style scoped></style>
